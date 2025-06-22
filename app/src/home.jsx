ba7c5a2dd949c592ea1737104ceaaaa487cac125
subiksha <subikshar.23eie@kongu.edu>
2025-06-22 16:14:05 +0530

    login page and day6 work

---
+++ app/src/home.jsx
@@ -0,0 +1,10 @@
+function Home(props){
+    let user=props.user;
+    console.log(user);
+    return (
+        <>
+        <h1> hi {user.username} welcome to the home</h1>
+        </>
+    )
+}
+export default Home;
